a9995f681643a97abad77201d73474a99ee876fb
[Nicolas Moncada Herrera] <[nmoncadaherrera@uoc.edu]>
2024-10-07 12:05:43 +0200

    Web Statica funcional con MONITORING + NETWORKS

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,7 +1,9 @@
+/********************************************************
+*                                                        *
+*              GENERAL WEBSITE: NAV (HOVER)              *
+*                                                        *
+********************************************************/
 
-
-
-//-------------------------MENU----------------------------------------------------------
 // Obtener todos los enlaces de navegación
 const navLinks = document.querySelectorAll(".headerNav__a");
 
@@ -38,14 +40,72 @@ navLinks.forEach((link) => {
   }
 });
 
-//---------------------------------------------MAP BOX MONITORING PAGE
 
-const map1 = document.querySelector('.mapBox__map1');
-const clock1 = document.querySelector('.clock1');
+/********************************************************
+*                                                        *
+*                MONITORING PAGE: INDEX                  *
+*                                                        *
+********************************************************/
+// Importar las imágenes usando require para que Parcel las procese
+//const clock1Image = require('../images/clock1.png'); // This form is used for the back with Node.js
+//const clock2Image = require('../images/clock2.png');
 
-const map2 = document.querySelector('.mapBox__map2');
-const clock2 = document.querySelector('.clock2');
+/*
+function initializeChart() {
+  const iframe = document.getElementById('dynamicIframe').contentWindow;
+  const chartContainer = iframe.document.getElementById('chart-container');
+}*/
 
+document.addEventListener('DOMContentLoaded', function() {
+  const monitoringSelector = document.getElementById('monitoringSelector');
+  const iframeContainer = document.getElementById('iframeContainer');  // Contenedor del iframe
+  const dynamicIframe = document.getElementById('dynamicIframe');  // El iframe dinámico para mostrar los gráficos
+
+  if (monitoringSelector) {
+      monitoringSelector.addEventListener('change', function() {
+          const selectedValue = this.value;
+
+          // Mapear las opciones del select a los gráficos HTML generados por Pyecharts
+          const chartPaths = {
+              MAC1_s4_time: './assets/images/MAC1_s4_time.html',
+              MAC1_sphi_time: './assets/images/MAC1_sphi_time.html',
+              MAC1_rotil1_time: './assets/images/MAC1_rotil1_time.html',
+
+              YELL_s4_time: './assets/images/YELL_s4_time.html',
+              YELL_sphi_time: './assets/images/YELL_sphi_time.html',
+              YELL_rotil1_time: './assets/images/YELL_rotil1_time.html'
+          };
+
+          // Si la selección está en el mapa, mostramos el gráfico correspondiente
+          if (chartPaths[selectedValue]) {
+              console.log('Ruta del gráfico:', chartPaths[selectedValue]);  // Verificar la ruta del gráfico
+              dynamicIframe.src = chartPaths[selectedValue];  // Cambiar el gráfico en el iframe
+              iframeContainer.style.display = 'block';  // Mostrar el contenedor del iframe
+              //dynamicIframe.style.display = 'block';  // Asegurarnos de que se muestre el iframe
+          } else {
+            iframeContainer.style.display = 'none';  // Ocultar el contenedor si no hay selección válida
+              //dynamicIframe.src = './assets/images/placeholder.png';  // Volver al placeholder si no hay selección
+          }
+      });
+      iframeContainer.style.display = 'none';
+  } else {
+      console.error("El elemento 'monitoringSelector' no fue encontrado en el DOM");
+  }
+});
+
+/********************************************************
+*                                                        *
+*                MONITORING PAGE: MAPS                   *
+*                                                        *
+********************************************************/
+const mapS4 = document.querySelector('.mapBox__s4H3');
+const s4 = document.querySelector('.mapBox__s4Img');
+
+const mapRotil = document.querySelector('.mapBox__rotilH3');
+const rotil = document.querySelector('.mapBox__rotilImg');
+
+const mapSphi = document.querySelector('.mapBox__sphiH3');
+const sphi = document.querySelector('.mapBox__sphiImg');
 
 // Función para alternar visibilidad de la imagen
 function toggleVisibility(img, icon) {
@@ -59,13 +119,52 @@ function toggleVisibility(img, icon) {
 }
 
 // Event listeners para los títulos
-map1.addEventListener('click', () => {
-    toggleVisibility(clock1);
+mapS4.addEventListener('click', () => {
+    toggleVisibility(s4);
 });
 
-map2.addEventListener('click', () => {
-    toggleVisibility(clock2);
+mapRotil.addEventListener('click', () => {
+    toggleVisibility(rotil);
 });
 
-//-------------------------------------------------------------------------
+mapSphi.addEventListener('click', () => {
+  toggleVisibility(sphi);
+});
+
+/********************************************************
+*                                                        *
+*                MONITORING PAGE: PAST RECORDS           *
+*                                                        *
+********************************************************/
+
+
+// Establecer la fecha máxima al día de hoy
+document.addEventListener("DOMContentLoaded", function () {
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, "0");
+  const mm = String(today.getMonth() + 1).padStart(2, "0"); // Enero es 0
+  const yyyy = today.getFullYear();
+  const formattedDate = `${yyyy}-${mm}-${dd}`;
+  document.getElementById("dateInput").setAttribute("max", formattedDate);
+});
+
+document.getElementById("dateInput").addEventListener("change", function () {
+  const selectedDate = new Date(this.value);
+  const month = selectedDate.getMonth(); // 0 = enero, 1 = febrero, ..., 7 = agosto, 8 = septiembre
+  const septiembreImage = document.getElementById("septiembreImage");
+  const agostoImage = document.getElementById("agostoImage");
+  // Ocultar ambas imágenes por defecto
+  septiembreImage.style.display = "none";
+  agostoImage.style.display = "none";
+  // Mostrar la imagen correspondiente
+  if (month === 7) {
+    // Agosto
+    agostoImage.style.display = "block";
+  } else if (month === 8) {
+    // Septiembre
+    septiembreImage.style.display = "block";
+  }
+});
+
+
 
